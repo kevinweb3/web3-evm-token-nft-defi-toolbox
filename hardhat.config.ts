ff072a2f8363242d38cd12481780ff98ebb0ca8b
kevinweb3 <kevinzhw@outlook.com>
2023-11-11 21:53:56 +0800

    commit

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,7 @@ import { HardhatUserConfig, extendEnvironment, extendProvider, task} from "hardh
 import "@nomicfoundation/hardhat-toolbox";
 require('dotenv').config();
 
-const PRIVATE_KEY: string = String(process.env.PRIVATE_KEY);
+const PRIVATE_KEY: string = String(process.env.NEXT_PUBLIC_PRIVATE_KEY);
 
 /*
   Hardhat内置的常用任务:
@@ -28,7 +28,7 @@ extendEnvironment((hre: any) => {
 });
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: "0.8.20",
   defaultNetwork: 'sepolia',
   networks: {
     hardhat: {
@@ -40,15 +40,15 @@ const config: HardhatUserConfig = {
       url: 'http://127.0.0.1:7545'
     },
     sepolia: {
-      url: "https://eth-sepolia.g.alchemy.com/v2/" + process.env.ALCHEMY_Sepolia_API_KEY,
+      url: "https://eth-sepolia.g.alchemy.com/v2/" + process.env.NEXT_PUBLIC_ALCHEMY_Sepolia_API_KEY,
       accounts: [PRIVATE_KEY],
     },
     goerli: {
-      url: "https://goerli.infura.io/v3/" + process.env.INFURA_API_KEY,
+      url: "https://goerli.infura.io/v3/" + process.env.NEXT_PUBLIC_INFURA_API_KEY,
       accounts: [PRIVATE_KEY],
     },
     mainnet: {
-      url: "https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY,
+      url: "https://mainnet.infura.io/v3/" + process.env.NEXT_PUBLIC_INFURA_API_KEY,
       accounts: [PRIVATE_KEY],
     }
   },
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,7 +1,5 @@
-import { artifacts, ethers } from "hardhat";
-
+import { ethers } from "hardhat";
 import fs from "fs";
-import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
 
 async function main() {
   const [deployer] = await ethers.getSigners();
@@ -10,59 +8,38 @@ async function main() {
 
   const contractFactory = await ethers.getContractFactory("SimpleToken");
   const deployContract = await contractFactory.deploy("SimpleToken", "ST", 100000000, 10);
-
-  // const deployContract = await ethers.deployContract("SimpleToken");
-
-  // console.log("11", await deployContract.symbol(), await deployContract.name());
-
   await deployContract.waitForDeployment();
 
   const contractAddress = await deployContract.getAddress();
   console.log("合约部署地址：", contractAddress);
 
-  // 获取合约信息保存传给前端
+  // 获取部署合约信息保存到本地文件，供前端合约交互使用
   const contractInfos = {
     contractName: await deployContract.name(),
     contractSymbol: await deployContract.symbol(),
     contractDecimals: String(await deployContract.decimals()),
     contractTotalSupply: String(await deployContract.totalSupply()),
     contractBalance: String(await deployContract.balanceOf(address)),
+    contractAddress,
   };
 
-  // 将合约地址和部署账户信息生成json文件传给前端frontend
-  saveFrontendFiles(contractAddress, deployer, contractInfos);
+  // 将合约地址和部署账户信息生成json文件传给前端
+  saveFrontendFiles("SimpleToken", contractInfos);
 }
 
 function saveFrontendFiles(
-  contractAddress: string,
-  deployerAccount: HardhatEthersSigner,
+  name: string,
   _contractInfos: { contractName: string; contractSymbol: string; contractDecimals: string; contractTotalSupply: string; contractBalance: string; }
 ) {
-  const contractsDir = __dirname + "/contracts-json";
+  const contractsAddressDir = "/../contractInfo";
 
-  if (!fs.existsSync(contractsDir)) {
-    fs.mkdirSync(contractsDir);
+  if (!fs.existsSync(contractsAddressDir)) {
+    fs.mkdirSync(contractsAddressDir);
   }
 
-  const TokenArtifact = artifacts.readArtifactSync("SimpleToken");
+  const path = contractsAddressDir + name + ".json";
 
-  const contractAddressDir = contractsDir + "/contractAddress.json";
-  const deployerAccountDir = contractsDir + "/deployerAccount.json";
-  const SimpleTokenDir = contractsDir + "/SimpleToken.json";
-  const _contractInfosDir = contractsDir + "/ContractInfos.json";
-
-  fs.writeFileSync(
-    contractAddressDir,
-    JSON.stringify({ contractAddress: contractAddress }, undefined, 2)
-  );
-
-  fs.writeFileSync(
-    deployerAccountDir,
-    JSON.stringify({ deployer: deployerAccount }, undefined, 2)
-  );
-
-  fs.writeFileSync(SimpleTokenDir, JSON.stringify(TokenArtifact, null, 2));
-  fs.writeFileSync(_contractInfosDir, JSON.stringify(_contractInfos, null, 2));
+  fs.writeFileSync(path, JSON.stringify(_contractInfos, null, 2));
 }
 
 main().catch((error) => {
